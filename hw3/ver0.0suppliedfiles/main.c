f826aabead9c8b52c2defe329675400a49dbb095
edlafem1 <edlafem1@umbc.edu>
2015-09-20 01:15:12 -0400

    added approximation code

--- hw3/ver0.0suppliedfiles/main.c
+++ hw3/ver0.0suppliedfiles/main.c
@@ -145,6 +145,29 @@ int main (int argc, char *argv[])
 			  printf(",");
 	  }
 	  printf(")\n");
+	  free(y);
+  }
+
+  // Problem 2 Section
+  double err = 1.0 + tol; // to ensure 1 pass through loop
+  int iterations = 0;
+  double lambdaold = 0, lambda = 0; //, norm_y; // not using norm_y
+  // just to make sure, in case we change the code above:
+  matrix_vector_mult_parallel(l_y, l_A, l_x, n, id, np);
+
+  while ((err > tol) & (iterations < itmax)) {
+	  ++iterations; // increment iteration counter
+	  lambdaold = lambda;
+
+	  norm_y = euclidean_norm_parallel(l_y, n, id, np); // Euclidean vector norm of vector y
+	  //x = y / normy; // scale y by its norm, such that x has norm 1
+	  matrix_vector_mult_parallel(l_y, l_A, l_x, n, id, np); // y_new = A * y_old
+	  lambda = dot_product_parallel(l_y, l_x, n, id, np) / dot_product_parallel(l_x, l_y, n, id, np); // eigenvalue approximation using Rayleigh quotient
+	  err = fabs((lambda - lambdaold) / lambda);
+  }
+
+  if (iterations == itmax && id == 0) {
+	  printf("Max number of iterations reached. Answer shown is the most recent approximation.\n");
   }
 
 
@@ -153,8 +176,6 @@ int main (int argc, char *argv[])
 
 
 
-
-  print_Square_Matrix(l_A, id, n, np);
   
   free_vector(l_y);
   free_vector(l_x);
