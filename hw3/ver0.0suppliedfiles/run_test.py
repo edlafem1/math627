ced0bb5abd65755fbf1e485e712cdaed7275a6fe
Edward LaFemina <edlafem1@maya-usr1.cm.cluster>
2015-09-25 02:11:18 -0400

    some stuff

--- hw3/ver0.0suppliedfiles/run_test.py
+++ hw3/ver0.0suppliedfiles/run_test.py
@@ -1,24 +1,24 @@
 import sys
 
-filename = "run_" + str(sys.argv[1]) + "_" + str(sys.argv[2]) + "_" + str(sys.argv[3]) + ".slurm"
+filename = "run_" + str(sys.argv[1]) + "_" + str(sys.argv[2]) + "_" + str(sys.argv[3]) + "_n=" + str(sys.argv[4]) + ".slurm"
 
 slurm_file = open(filename, "w")
 
 slurm_file.write("#!/bin/bash\n")
-slurm_file.write("#SBATCH --job-name=" + str(sys.argv[1]) + "\n")
-slurm_file.write("#SBATCH --output=" + str(sys.argv[1]) + "_" + str(sys.argv[2]) + "_" + str(sys.argv[3]) + ".out\n")
-slurm_file.write("#SBATCH --error=" + str(sys.argv[1]) + "_" + str(sys.argv[2]) + "_" + str(sys.argv[3]) + ".err\n")
-slurm_file.write("#SBATCH --partition=develop\n")
+slurm_file.write("#SBATCH --job-name=" + str(sys.argv[1]) + str(sys.argv[4]) + "\n")
+slurm_file.write("#SBATCH --output=" + str(sys.argv[1]) + "_" + str(sys.argv[2]) + "_" + str(sys.argv[3]) + "_n=" + str(sys.argv[4]) + ".out\n")
+slurm_file.write("#SBATCH --error=" + str(sys.argv[1]) + "_" + str(sys.argv[2]) + "_" + str(sys.argv[3]) + "_n=" + str(sys.argv[4]) + ".err\n")
+slurm_file.write("#SBATCH --partition=batch\n")
 slurm_file.write("#SBATCH --nodes=" + str(sys.argv[2]) + "\n")
 slurm_file.write("#SBATCH --ntasks-per-node=" + str(sys.argv[3]) + "\n")
 slurm_file.write("#SBATCH --exclusive\n")
 slurm_file.write("#SBATCH --constraint=hpcf2013\n")
+slurm_file.write("#SBATCH --qos=short\n")
 slurm_file.write("\n")
 slurm_file.write("srun ./" + str(sys.argv[1]))
 if len(sys.argv) > 4:
-	for i in range(4,len(sys.argv)):
-		slurm_file.write(" " + str(sys.argv[i]))
-slurm_file.write("\n")
+        for i in range(4,len(sys.argv)):
+                slurm_file.write(" " + str(sys.argv[i]))
 
 slurm_file.close()
 
