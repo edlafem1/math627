6a1667e09fdf14afc4adfc0b1963ff068f14a4ee
edlafem1 <edlafem1@umbc.edu>
2015-10-09 22:45:12 -0400

    really removed parallel

--- hw4/hw4/utilities.c
+++ hw4/hw4/utilities.c
@@ -56,7 +56,21 @@ void matrix_vector_mult(double *l_y, double *l_A, double *l_x, double *temp_y, d
 void print_Matrix(double *l_matrix, int m, int n, int id, int np) {
 	// create a matrix A for use on only process 0 to gather all local pieces into one place
 	double *A;
-	/*
+
+	A = l_matrix;
+
+	int row, col;
+	for (row = 0; row < m; row++) {
+		for (col = 0; col < n; col++) {
+			printf("% -24.16e   ", A[row + col * n]);
+		}
+		printf("\n");
+	}
+}
+
+void trash_print_Matrix(double *l_matrix, int m, int n, int id, int np) {
+	// create a matrix A for use on only process 0 to gather all local pieces into one place
+	double *A;
 #ifdef PARALLEL
 	int destination = 0;
 	if (id == 0) {
@@ -67,14 +81,11 @@ void print_Matrix(double *l_matrix, int m, int n, int id, int np) {
 	// gather all l_A which have been set in the setup_example function into A for printing by process 0
 	MPI_Gather(l_matrix, m*l_n, MPI_DOUBLE, A, m*l_n, MPI_DOUBLE, destination, MPI_COMM_WORLD);
 #else
-	*/
 	A = l_matrix;
-	/*
 #endif
 #ifdef PARALLEL
 	if (id == destination) {
 #endif
-		*/
 		int row, col;
 		for (row = 0; row < m; row++) {
 			for (col = 0; col < n; col++) {
@@ -82,10 +93,8 @@ void print_Matrix(double *l_matrix, int m, int n, int id, int np) {
 			}
 			printf("\n");
 		}
-		/*
 #ifdef PARALLEL
 		free(A); // only allocated on process destination=0
 	}
 #endif
-*/
 }
