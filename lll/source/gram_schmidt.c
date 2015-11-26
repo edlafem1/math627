8befeecd7866d2c782ce8a1b5a47225b91c4616b
edlafem1 <edlafem1@umbc.edu>
2015-11-26 12:11:00 -0500

    A version of GramSchmidt is working.

--- lll/source/gram_schmidt.c
+++ lll/source/gram_schmidt.c
@@ -1,5 +1,9 @@
-int dot_product(int *x, int *y, int length) {
-    int product = 0;
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+double dot_product(double *x, double *y, int length) {
+    double product = 0;
 
     for (int i = 0; i < length; i++) {
         product += (x[i] * y[i]);
@@ -7,23 +11,41 @@ int dot_product(int *x, int *y, int length) {
     return product;
 }
 
+double euclidean_norm(double *x, int m) {
+    return sqrt(dot_product(x, x, m));
+}
+
+void uj_projection(double *ui, double *uj, int m) {
+    double numerator, denominator, quotient;
+    numerator = dot_product(ui, uj, m);
+    denominator = dot_product(ui, ui, m);
+    quotient = numerator / denominator;
+
+    for (int i = 0; i < m; i++) {
+        uj[i] -= (quotient * ui[i]);
+    }
+}
+
 /*
 B is column major matrix of dimension m x n
 */
-void gramschmidt_process(int **B, int m, int n) {
-    int *u_k, *u_j; // represents vectors
-    int numerator, denominator;
+void gramschmidt_process(double *B, int m, int n) {
+    double norm;
 
-    for (int k = 0; k < n; k++) {
-        u_k = B[k*m];
-        
-        for (int j = 0; j < k - 1; j++) {
-            u_j = B[j*m];
-            numerator = dot_product(u_k, u_j, m);
-            denominator = dot_product(u_j, u_j, m);
-            for (int l = 0; l < m; l++) {
-                u_k[l] -= ((numerator / denominator)*u_j[l]);
-            }
+    double *ui, *uj;
+
+    for (int i = 0; i < n; i++) {
+        ui = &(B[i*m]);
+        norm = euclidean_norm(ui, m);
+        for (int iindex = 0; iindex < m; iindex++) {
+            ui[iindex] /= norm;
         }
+
+        for (int j = i + 1; j < n; j++) {
+            uj = &(B[j*m]);
+            uj_projection(ui, uj, m); //comment says remove component in direction ui
+        }
+
     }
+    
 }
