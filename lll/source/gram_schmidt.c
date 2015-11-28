959287d7982ed943a831e85c2d12cd206fee37a9
edlafem1 <edlafem1@umbc.edu>
2015-11-28 00:12:55 -0500

    QDU decomposition is working. See comment about speed in qdu_decomposition()

--- lll/source/gram_schmidt.c
+++ lll/source/gram_schmidt.c
@@ -31,25 +31,50 @@ A is column major matrix of dimension m x n
 */
 void gramschmidt_process(double *A, double *E, int m, int n) {
     double norm;
-
     double *vi, *vj;
 
+    memcpy(E, A, sizeof(double)*m*n);
+
     for (int i = 0; i < n; i++) {
-        vi = &(A[i*m]);
+        vi = &(E[i*m]);
 
         norm = euclidean_norm(vi, m);
         for (int iindex = 0; iindex < m; iindex++) {
-            E[i*m + iindex] = vi[iindex] / norm;
+            vi[iindex] /= norm;
         }
 
         for (int j = i + 1; j < n; j++) {
-            vj = &(A[j*m]);
-            vj_projection(&(E[i*m]), vj, m); //comment says remove component in direction vi
+            vj = &(E[j*m]);
+            vj_projection(vi, vj, m); //comment says remove component in direction vi
         }
 
     }
 }
 
-void qr_decomposition(double *A, double *E, int m, int n) {
+void qdu_decomposition(double *A, double *E, double *D, double *U, int m, int n) {
+    double denominator;
+    // see if maybe switching order of the loops and doing condition i <= j will be faster
+    for (int i = 0; i < m; i++) {
+        denominator = dot_product(&(E[i*m]), &(A[i*m]), m); // this is R[i,i]
+        for (int j = i; j < m; j++) {
+            if (i == j) {
+                // we use this indexing because j is changing most frequently and we reduce cache misses. Note, we can switch i and j here because i==j
+                U[i*m + j] = 1;
+                D[i*m + j] = denominator; // D[i,j] = R[i,i]
+            }
+            else {
+                /*
+                printf("upper diagonal %i %i\n", i, j);
+                for (int z = 0; z < m; z++) {
+                    printf("%f\t%f\n", E[i*m + z], A[j*m + z]);
+                }
+                printf("<Ei,Aj>=%f\n", dot_product(&(E[i*m]), &(A[j*m]), m));
+                printf("Res = %f\n", dot_product(&(E[i*m]), &(A[j*m]), m) / denominator);
+                */
+                
+                U[j*m + i] = dot_product(&(E[i*m]), &(A[j*m]), m) / denominator; // U[i,j] = R[i,j] / R[i,i]
+            }
 
+        }
+    }
 }
