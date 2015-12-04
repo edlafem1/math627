bff363200ddc8e99626e975e43f617329cccd834
edlafem1 <edlafem1@umbc.edu>
2015-12-04 00:41:34 -0500

    changed variable names in gram_schmidt.c

--- lll/source/gram_schmidt.c
+++ lll/source/gram_schmidt.c
@@ -32,21 +32,21 @@ void normalize(double *v, int m) {
     }
 }
 /*
-A is column major matrix of dimension m x n
-Note: what if B in m x n is such that m > n? Does the bottom part of E mean anything? We want E n x n, but for now it is m x n because we have not answered this question.
+B is column major matrix of dimension m x n
+Note: what if B in m x n is such that m > n? Does the bottom part of Q mean anything? We want Q n x n, but for now it is m x n because we have not answered this question.
 */
-void gramschmidt_process(double *A, double *E, int m, int n) {
+void gramschmidt_process(double *B, double *Q, int m, int n) {
     double *vi, *vj;
 
-    memcpy(E, A, sizeof(double)*m*n);
+    memcpy(Q, B, sizeof(double)*m*n);
 
     for (int i = 0; i < n; i++) {
-        vi = &(E[i*m]);
+        vi = &(Q[i*m]);
 
         normalize(vi, m);
 
         for (int j = i + 1; j < n; j++) {
-            vj = &(E[j*m]);
+            vj = &(Q[j*m]);
             vj_projection(vi, vj, m); //comment says remove component in direction vi
         }
 
@@ -56,11 +56,11 @@ void gramschmidt_process(double *A, double *E, int m, int n) {
 /*
 See above considerations about dimension of other matrices
 */
-void qdu_decomposition(double *A, double *E, double *D, double *U, int m, int n) {
+void qdu_decomposition(double *B, double *Q, double *D, double *U, int m, int n) {
     double denominator;
     // see if maybe switching order of the loops and doing condition i <= j will be faster
     for (int i = 0; i < m; i++) {
-        denominator = dot_product(&(E[i*m]), &(A[i*m]), m); // this is R[i,i]
+        denominator = dot_product(&(Q[i*m]), &(B[i*m]), m); // this is R[i,i]
         for (int j = i; j < m; j++) {
             if (i == j) {
                 // we use this indexing because j is changing most frequently and we reduce cache misses. Note, we can switch i and j here because i==j
@@ -68,7 +68,7 @@ void qdu_decomposition(double *A, double *E, double *D, double *U, int m, int n)
                 D[i] = denominator*denominator; // (D[i,j] = R[i,i], but D is only diagonal, so we represent it with just a vector)^2 for LLL algo
             }
             else {                
-                U[j*m + i] = dot_product(&(E[i*m]), &(A[j*m]), m) / denominator; // U[i,j] = R[i,j] / R[i,i]
+                U[j*m + i] = dot_product(&(Q[i*m]), &(B[j*m]), m) / denominator; // U[i,j] = R[i,j] / R[i,i]
             }
 
         }
