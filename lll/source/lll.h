02e2bc1a91bca219c128f6d12aaed6f59dc0fb14
edlafem1 <edlafem1@umbc.edu>
2015-12-05 17:22:47 -0500

    some printing stuff

--- lll/source/lll.h
+++ lll/source/lll.h
@@ -11,6 +11,8 @@ int size_reduced(double *U, int m, int n);
 
 int LLL_reduced(double *D, double *U, double w, int m, int n);
 
+void reduce(double *U, double *B, double *M, int i, int j, int m, int n);
+
 void LLL(double *B, double *D, double *U, double *M, double w, int m, int n);
 
 #endif
