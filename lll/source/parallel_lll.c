c9a5768d0d6ac52bebdbe29b7d81cc5e0ba90874
edlafem1 <edlafem1@umbc.edu>
2015-12-10 01:27:51 -0500

    this will fail amazingly

--- lll/source/parallel_lll.c
+++ lll/source/parallel_lll.c
@@ -1,24 +1,134 @@
 #include "parallel_lll.h"
 
+void parallel_reduceSwapRestore(int i, int gamma, double *B, double *D, double *U, double *M, int m, int n, int id, int np, int do_work) {
+
+
+    if (do_work != 1) return; // no need to permute these columns
+
+    // Update i-1 and i columns of B
+    double tempB;
+    for (int k = 0; k < m; ++k) {
+        tempB = B[(i - 1)*m + k];
+        B[(i - 1)*m + k] = B[i*m + k] - gamma*tempB;
+        B[i*m + k] = tempB;
+    }
+
+    //Update i-1 and i columns of M
+    double tempM;
+    for (int k = 0; k < n; ++k) {
+        tempM = M[(i - 1)*n + k];
+        M[(i - 1)*n + k] = M[i*m + k] - gamma*tempM;
+        M[i*m + k] = tempM;
+    }
+
+    // Update i-1 and i columsn of U
+    double tempU;
+    for (int k = 0; k <= i - 2; ++k) {
+        tempU = U[(i - 1)*n + k];
+        U[(i - 1)*n + k] = U[i*n + k] - gamma*tempU;
+        U[i*n + k] = tempU;
+    }
+}
+
+
 void parallel_LLL(double *B, double *D, double *U, double *M, double w, int m, int n, int id, int np) {
 
     int k = 1;
     int gamma;
     int f = 0;
+    MPI_Datatype row_t;
+    MPI_Type_vector(n, 1, n, MPI_DOUBLE, &row_t);
+    MPI_Type_commit(&row_t);
+
     while (f == 0) {
+        int how_many = (2 + (id + 1) * (n / np) - 1) - (2 + id * (n / np) - 1);
+        int *works = (int *)calloc(how_many, sizeof(int));
+        int counter = 0;
         f = 1;
         // Even math k (odd in computer terms). Math says start at 2, which is 1 in computer terms
         // Begin Parallel
-        for (int k = 2 + id * (n / np) - 1; k < 1  + (id + 1) * (n / np) - 1 && k < n; k += 2) {
+        for (int k = 2 + id * (n / np) - 1; k < 2  + (id + 1) * (n / np) - 1 && k < n; k += 2) {
             gamma = closest_integer(U[k*n + (k - 1)]);
-            if (D[k] < (w -
-                (U[k*n + (k - 1)] - gamma)
-                *(U[k*n + (k - 1)] - gamma)
-                )*D[k - 1]) {
+            int do_work = 0;
+            if (D[k] < (w - (U[k*n + (k - 1)] - gamma)*(U[k*n + (k - 1)] - gamma))*D[k - 1]) {
                 f = 0;
-                reduceSwapRestore(k, gamma, B, D, U, M, m, n);
+                works[counter] = 1;
+                do_work = 1;
+                //reduceSwapRestore(k, gamma, B, D, U, M, m, n);
+            }
+            counter++;
+            //////////////////////////////////////////////////////////////////////
+            double u, d_hat_m, epsilon;
+
+            if (do_work == 1) {
+                u = U[k*n + (k - 1)];
+                d_hat_m = D[k] + (u - gamma)*(u - gamma)*D[k - 1];
+                D[k] = (D[k - 1] * D[k]) / d_hat_m;
+
+                epsilon = ((u - gamma)*D[k - 1]) / d_hat_m;
+                D[k - 1] = d_hat_m;
+
+                // Do U=X^-1 * U
+                u = U[k*n + (k - 1)];
+                double u1, u2;
+                for (int z = k + 1; z < n; ++z) {
+                    u1 = U[z*n + (k - 1)];
+                    u2 = U[z*n + k];
+                    U[z*n + (k - 1)] = u1*epsilon + (1 - epsilon*u + gamma*epsilon)*u2;
+                    U[z*n + k] = u1 + (gamma - u)*u2;
+                }
+                U[k*n + (k - 1)] = epsilon;
+            }
+
+            ////////////////////////////////////////////////////////////////////////
+        }
+        MPI_Allgather(&(U[id * (n / np)*n]), how_many, row_t, U, how_many, row_t, MPI_COMM_WORLD);
+        ////////////////////////////////////////////////////////////////////////////
+        counter = 0;
+        for (int k = 2 + id * (n / np) - 1; k < 2 + (id + 1) * (n / np) - 1 && k < n; k += 2) {
+            gamma = closest_integer(U[k*n + (k - 1)]);
+            int do_work = 0;
+            if (works[counter]==1) {
+                f = 0;
+                do_work = 1;
+                //reduceSwapRestore(k, gamma, B, D, U, M, m, n);
+            }
+            counter++;
+            //////////////////////////////////////////////////////////////////////
+            double u, d_hat_m, epsilon;
+
+            if (do_work == 1) {
+                double tempB;
+                for (int z = 0; z < m; ++z) {
+                    tempB = B[(k - 1)*m + z];
+                    B[(k - 1)*m + z] = B[k*m + z] - gamma*tempB;
+                    B[k*m + z] = tempB;
+                }
+
+                //Update k-1 and k columns of M
+                double tempM;
+                for (int z = 0; z < n; ++z) {
+                    tempM = M[(k - 1)*n + z];
+                    M[(k - 1)*n + z] = M[k*m + z] - gamma*tempM;
+                    M[k*m + z] = tempM;
+                }
+
+                // Update k-1 and k columsn of U
+                double tempU;
+                for (int z = 0; z <= k - 2; ++z) {
+                    tempU = U[(k - 1)*n + z];
+                    U[(k - 1)*n + z] = U[k*n + z] - gamma*tempU;
+                    U[k*n + z] = tempU;
+                }
+                
             }
         }
+            ////////////////////////////////////////////////////////////////////////////////
+        MPI_Allgather(&(U[id * (n / np)*n]), how_many*n, MPI_DOUBLE, U, how_many*n, MPI_DOUBLE, MPI_COMM_WORLD);
+        MPI_Allgather(&(M[id * (n / np)*n]), how_many*n, MPI_DOUBLE, M, how_many*n, MPI_DOUBLE, MPI_COMM_WORLD);
+        MPI_Allgather(&(B[id * (n / np)*m]), how_many*m, MPI_DOUBLE, B, how_many*m, MPI_DOUBLE, MPI_COMM_WORLD);
+        MPI_Allgather(&(D[id*(n / np)*1])  , how_many*1, MPI_DOUBLE, D, how_many*1, MPI_DOUBLE, MPI_COMM_WORLD);
+                
         // End Parallel
 #ifdef MPI_INCLUDE
         MPI_Barrier(MPI_COMM_WORLD);
