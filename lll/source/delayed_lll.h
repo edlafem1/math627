3a9ad1651548e7d46698070a3afc342bc60e91b5
edlafem1 <edlafem1@umbc.edu>
2015-12-10 01:34:28 -0500

    dont know how function declaration got lost

--- lll/source/delayed_lll.h
+++ lll/source/delayed_lll.h
@@ -7,4 +7,6 @@
 
 void delayed_LLL(double *B, double *D, double *U, double *M, double w, int m, int n);
 
+void reduceSwapRestore(int i, int gamma, double *B, double *D, double *U, double *M, int m, int n);
+
 #endif
