6f820ffb521ee4c7cd62316a5be5f3f70a8af08b
edlafem1 <edlafem1@umbc.edu>
2015-12-10 01:48:18 -0500

    in place gather

--- lll/source/parallel_lll.c
+++ lll/source/parallel_lll.c
@@ -52,7 +52,12 @@ void parallel_LLL(double *B, double *D, double *U, double *M, double w, int m, i
             ////////////////////////////////////////////////////////////////////////
         }
         printf("[%i] Before first allgather\n", id);
-        MPI_Allgather(&(U[id * (n / np)*n]), how_many, row_t, U, how_many, row_t, MPI_COMM_WORLD);
+        if (id == 0) {
+            MPI_Allgather(MPI_IN_PLACE, how_many, row_t, U, how_many, row_t, MPI_COMM_WORLD);
+        }
+        else {
+            MPI_Allgather(&(U[id * (n / np)*n]), how_many, row_t, U, how_many, row_t, MPI_COMM_WORLD);
+        }
         printf("[%i] After first allgather\n", id);
         ////////////////////////////////////////////////////////////////////////////
         counter = 0;
